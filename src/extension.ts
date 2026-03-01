50e9f47bd9af6903e4993e0532654c0fe1e5d9fc
Ksawery Radziwiłowicz <ksawery.radziwilowicz@gmail.com>
2026-03-02 00:36:19 +0100

    fix ts project

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,29 @@
 import * as path from 'path';
 import * as vscode from 'vscode';
 import * as net from 'net';
+import { ChildProcess, exec } from 'child_process';
+import * as readline from 'readline';
 
 import { LanguageClient, LanguageClientOptions, ServerOptions, StreamInfo, TransportKind } from 'vscode-languageclient/node';
 
 let client: LanguageClient;
+let serverProcess: ChildProcess;
 
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
     // https://github.com/microsoft/vscode-languageserver-node/issues/662
     // ???
-    console.log('Activating WebDSL extension!');
+    console.log('Activating WebDSL extension!!!');
+    // serverProcess = exec("java -jar /home/ksaweryr/uni/weblab/LspSandbox/app/build/libs/webdsl-lsp-1.0.0.jar");
+    // const rl = readline.createInterface({ input: serverProcess.stdout! });
+    // const line: string = await new Promise((resolve, reject) => {
+    //     rl.once('line', resolve);
+    // });
+    // console.log(`Received line: ${line}`);
+    // const port = parseInt(line.match(/port (\d+)!/)![1]);
+    const port = 1337;
     let serverOptions = () => {
         console.log('WebDSL extension connecting!');
-        let socket = net.connect({ port: 1337, host: "0" });
+        let socket = net.connect({ port, host: "0" });
         let result: StreamInfo = {
             writer: socket,
             reader: socket
@@ -37,9 +48,11 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 export function deactivate() {
-    if (!client) {
-        return undefined;
+    const result = client ? client.stop() : undefined;
+
+    if (serverProcess) {
+        serverProcess.kill();
     }
 
-    return client.stop();
+    return result;
 }
